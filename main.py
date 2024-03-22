0e4d34af82cda40a1168b3cb2cd8898704b9e4d6
Brynn LeBlanc <brynn.e.leblanc@vanderbilt.edu>
2024-03-22 10:26:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -670,7 +670,7 @@ def main():
                                     "\", \"):") or " "
         forbidden_locations_list = forbidden_locations.split(", ")
         checkLists(required_locations_list, forbidden_locations_list)
-
+        """This is a problem because we dont check for duplicates and change the no_duplicates function"""
 
     """
     option for soft forbidden location
